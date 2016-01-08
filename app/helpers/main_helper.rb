771d28ca73bec2762fc07a40246969abfcd70da1
seventhsense <seventh@scimpr.com>
2016-01-08 18:18:16 +0900

    improve view

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -11,14 +11,14 @@ module MainHelper
     end
   end
 
-  def thumb_link thumb_path, link_path
+  def thumb_link thumb_path, link_path, height=64
     if File.exist? 'public/' + thumb_path
-      link_to image_tag(thumb_path), link_path, target: '_blank'
+      link_to image_tag(thumb_path, height:height), link_path, target: '_blank'
     elsif File.extname(thumb_path) == '.doc'
-      link_to image_tag('/Microsoft Office 2003 Word.png', height:64), link_path, target: '_blank'
+      link_to image_tag('/Microsoft Office 2003 Word.png', height:height), link_path, target: '_blank'
     elsif File.extname(thumb_path) == '.docx'
 
-      link_to image_tag('/Microsoft Office 2007 Word.png', height:64), link_path, target: '_blank'
+      link_to image_tag('/Microsoft Office 2007 Word.png', height:height), link_path, target: '_blank'
     else
       link_to 'download', link_path, target: '_blank'
     end
